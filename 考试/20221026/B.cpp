6df4a9b87fabd835f5edde8216f5870237e4ead4
baijingren <2264722763@qq.com>
2022-10-26 11:53:21 +0000

    20221026

--- 考试/20221026/B.cpp
+++ 考试/20221026/B.cpp
@@ -1,43 +1,28 @@
-#include <bits/stdc++.h>
-//#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 const int qwe=2e6+5;
 int n,m,T;
-int ans=0;
-int a[qwe],s[qwe];
-//vector<int> e;
-int main() {
+int a[qwe];
+int s[qwe];
+int dp[qwe][2];
+int main(){
 	freopen("sum.in","r",stdin);
 	freopen("sum.out","w",stdout);
-//	freopen("sum.log","w",stderr);
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 	cin>>T;
+	int ans=0;
 	while(T--){
 		cin>>n>>m;
-		memset(a,0,sizeof(a));
-		memset(s,0,sizeof(s));
-		ans=0;
+		int tmp=0;
 		for(int i=1;i<=n;i++){
 			cin>>a[i];
-		}
-		int st=0,tmp=0;
-		for(int i=1;i<=10;i++){
-			cerr<<a[i]<<' ';
-		}
-		cerr<<endl;
-		for(int i=1;i<=n;i++){
-			if(a[i]<0 && a[i+1]>0 && st==0){
-//				cerr<<a[i]<<' '<<a[i+1]<<endl;
-				st=i;
-			}
+			s[i]=s[i-1]+a[i];
 			if(a[i]>=0){
 				tmp++;
 			}
 		}
-		cerr<<st<<endl;
-		cerr<<a[st]<<' '<<a[st+1]<<endl;
 		if(tmp<=m){
 			sort(a+1,a+n+1,[](int a,int b){
 				return a>b;
@@ -45,38 +30,34 @@ int main() {
 			for(int i=1;i<=m;i++){
 				ans+=a[i];
 			}
-			cout<<ans<<'\n';
+			cout<<ans<<endl;
 			continue;
 		}
+		memset(dp,0,sizeof(dp));
+		for(int i=1;i<=m;i++){
+			int nw=i&1;
+			for(int j=1;j<=n;j++){
+				dp[j][nw]=max(dp[j-1][nw^1],dp[j-1][nw])+a[j];
+			}
+			for(int j=1;j<=n;j++){
+				dp[j][nw]=max(dp[j][nw],dp[j-1][nw]);
+			}
+		}
+		ans=dp[n][m&1];
+		memset(dp,0,sizeof(dp));
 		for(int i=1;i<=n;i++){
-			a[n+i]=a[i];
+			dp[i][0]=s[i];
 		}
-		int cnt=0;
-		for(int i=st;i<=n*2;i++){
-			if(a[i]<0){
-				if(i>n){
-					break;
-				}
-				cnt++;
-//				s[cnt]=a[i];
+		for(int i=1;i<=m;i++){
+			int nw=i&1;
+			for(int j=1;j<=n;j++){
+				dp[j][nw^1]=max(dp[j][nw^1],dp[j-1][nw^1]);
 			}
-			else{
-				s[cnt]+=a[i];
+			for(int j=1;j<=n;j++){
+				dp[j][nw]=max(dp[j-1][nw^1],dp[j-1][nw])+a[j];
 			}
 		}
-		sort(s+1,s+cnt+1,[](int a,int b){
-			return a>b;
-		});
-		if(cnt>m){
-			for(int i=1;i<=m;i++){
-				ans+=s[i];
-			}
-		}
-		else{
-			for(int i=1;i<=cnt;i++){
-				ans+=s[i];
-			}
-		}
-		cout<<ans<<'\n';
+		cout<<max(ans,dp[n][m&1])<<endl;
 	}
+	return 0;
 }
